6c512ed0ef7c9fc388a98eb92b7d99d7c521c542
xdab <przemyslawxdab@gmail.com>
2024-04-14 21:56:21 +0200

    audmod begin receiving KISS commands

--- apps/audmod/audmod.c
+++ apps/audmod/audmod.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include <configstuff/config.h>
 #include <netstuff/netclient.h>
@@ -20,11 +21,14 @@ ms_linecode_nrzi_decoder_t nrzi_decoder;
 hs_ax25_deframer_t ax25_deframer;
 
 ns_server_t kiss_server;
+hs_kiss_decoder_t kiss_decoder;
+pthread_mutex_t kiss_decoder_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void data_callback(void *data, uint32_t length);
 void kiss_data_start_callback();
 void kiss_data_callback(void *data, uint32_t length);
 void kiss_data_end_callback();
+void incoming_kiss_message_callback(hs_kiss_message_t *message);
 
 int main(int argc, const char *argv[])
 {
@@ -59,45 +63,51 @@ int main(int argc, const char *argv[])
     baud_rate = atof(cs_config_get_or_exit(&config, "BAUD"));
     kiss_port = atoi(cs_config_get_or_exit(&config, "KISSPORT"));
 
+    // Initialize modem and data link layer components
     if (ms_fsk_detector_init(&fsk_detector, mark_freq, space_freq, baud_rate, sample_rate))
     {
         fprintf(stderr, "Error: ms_fsk_detector_init() failed\n");
         return EXIT_FAILURE;
     }
-
     ms_bit_detector_init(&bit_detector, sample_rate, baud_rate);
     ms_linecode_nrzi_decoder_init(&nrzi_decoder);
     hs_ax25_deframer_init(&ax25_deframer);
 
-    if (ns_server_init(&kiss_server, kiss_port))
-    {
-        fprintf(stderr, "Error: ns_server_init() failed\n");
-        return EXIT_FAILURE;
-    }
-
-    ns_server_set_callbacks(&kiss_server, &kiss_data_start_callback, &kiss_data_callback, &kiss_data_end_callback);
-
+    // Initialize audio server
     if (ns_client_init(&client))
     {
         fprintf(stderr, "Error: ns_client_init() failed\n");
         return EXIT_FAILURE;
     }
-
     ns_client_set_callbacks(&client, &data_callback);
 
+    // Initialize KISS server
+    hs_kiss_decoder_init(&kiss_decoder, &incoming_kiss_message_callback);
+    if (ns_server_init(&kiss_server, kiss_port))
+    {
+        fprintf(stderr, "Error: ns_server_init() failed\n");
+        return EXIT_FAILURE;
+    }
+    ns_server_set_callbacks(&kiss_server, &kiss_data_start_callback, &kiss_data_callback, &kiss_data_end_callback);
+
+    // Connect to the audio server
     if (ns_client_connect(&client, host, port))
     {
         fprintf(stderr, "Error: ns_client_connect() failed\n");
         return EXIT_FAILURE;
     }
-
     fprintf(stderr, "Connected to %s:%d\n", host, port);
 
+    // Start KISS server
     ns_server_start(&kiss_server);
     fprintf(stderr, "KISS server started\n");
 
+    // Wait for KISS server thread
     ns_server_wait(&kiss_server);
 
+    fprintf(stderr, "Exiting...\n");
+
+    // Cleanup
     ms_fsk_detector_destroy(&fsk_detector);
     ns_client_destroy(&client);
     ns_server_destroy(&kiss_server);
@@ -127,8 +137,7 @@ void data_callback(void *data, uint32_t length)
         {
             // Print the packet to stderr
             hs_ax25_frame_pack_tnc2(&frame, buf);
-            fputs(buf, stderr);
-            fputs("\r\n", stderr);
+            fprintf(stderr, "RX %s \n", buf);
 
             // Send the packet to the KISS clients
             kiss_message.port = 0;
@@ -142,13 +151,33 @@ void data_callback(void *data, uint32_t length)
 
 void kiss_data_start_callback()
 {
+    pthread_mutex_lock(&kiss_decoder_mutex);
 }
 
 void kiss_data_callback(void *data, uint32_t length)
 {
-    fprintf(stderr, "Received %d bytes of KISS data\n", length);
+    int i;
+
+    for (i = 0; i < length; i++)
+        hs_kiss_decoder_process(&kiss_decoder, ((hs_byte *)data)[i]);
 }
 
 void kiss_data_end_callback()
 {
+    pthread_mutex_unlock(&kiss_decoder_mutex);
+}
+
+void incoming_kiss_message_callback(hs_kiss_message_t *message)
+{
+    char buf[512];
+    hs_ax25_frame_t frame;
+
+    if (message->command == KISS_DATA_FRAME)
+    {
+        hs_ax25_frame_unpack(&frame, message->data, message->data_length);
+
+        // Print the packet to stderr
+        hs_ax25_frame_pack_tnc2(&frame, buf);
+        fprintf(stderr, "TX %s \n", buf);
+    }
 }
